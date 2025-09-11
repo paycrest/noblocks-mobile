fe75cb4ebe58e39af3fabbf73715932bb32ba796
marvelous-ikechi <marvelike7@gmail.com>
2025-09-11 16:33:39 +0100

    Styled components updated

--- components/StyledComponents.ts
+++ components/StyledComponents.ts
@@ -1,9 +1,36 @@
-import { remapProps } from "nativewind";
+import { styled } from "nativewind";
+import { FlatList, ScrollView, SectionList } from "react-native";
+import ReanimatedSwipeable from "react-native-gesture-handler/ReanimatedSwipeable";
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 
-export const StyledKeyboardAwareScrollView = remapProps(
-  KeyboardAwareScrollView,
-  {
-    contentContainerStyle: "contentContainerClassName",
-  }
-);
+export const StyledKeyboardAwareScrollView = styled(KeyboardAwareScrollView, {
+  props: {
+    contentContainerStyle: true,
+  },
+});
+
+export const StyledScrollView = styled(ScrollView, {
+  props: {
+    contentContainerStyle: true,
+  },
+});
+
+export const StyledFlatList = styled(FlatList<any>, {
+  props: {
+    contentContainerStyle: true,
+    columnWrapperStyle: true,
+    ListFooterComponentStyle: true,
+  },
+});
+
+export const StyledSectionList = styled(SectionList<any>, {
+  props: {
+    contentContainerStyle: true,
+  },
+});
+
+export const StyledReanimatedSwipeable = styled(ReanimatedSwipeable, {
+  props: {
+    containerStyle: true,
+  },
+});
