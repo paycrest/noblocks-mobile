a2f9f523c53b00369b07e94d5d793bbdd79de89a
marvelous-ikechi <marvelike7@gmail.com>
2025-09-26 13:42:25 +0100

    added implementation for square icon

--- components/Check.tsx
+++ components/Check.tsx
@@ -1,33 +1,37 @@
-import React, { FunctionComponent } from "react";
-
-import { Colors } from "@/constants/Colors";
-import { useAppColorScheme } from "@/hooks/useAppColorScheme";
+import { useThemeColors } from "@/hooks/useThemeColor";
 import { Circle } from "lucide-react-native";
+import React from "react";
+import { Pressable } from "react-native";
+import CheckSquareIcon from "./svgs/check-square-icon";
 import DarkCheckMark from "./svgs/dark-checkMark";
+import UncheckedSquareIcon from "./svgs/unchecked-square-icon";
 
 interface Props {
   checked: boolean;
+  type?: "square" | "circle";
+  onPress?: () => void;
 }
 
-const Check: FunctionComponent<Props> = ({ checked }) => {
-  const colorScheme = useAppColorScheme();
+const Check: React.FC<Props> = ({ checked, type = "circle", onPress }) => {
+  const colors = useThemeColors();
 
-  return (
-    <>
-      {checked ? (
+  const renderIcon = () => {
+    if (type === "circle") {
+      return checked ? (
         <DarkCheckMark />
       ) : (
-        <Circle
-          color={
-            colorScheme === "dark"
-              ? Colors.dark.secondary
-              : Colors.light.secondary
-          }
-          size={20}
-        />
-      )}
-    </>
-  );
+        <Circle color={colors.secondary} size={20} />
+      );
+    }
+
+    if (type === "square") {
+      return checked ? <CheckSquareIcon /> : <UncheckedSquareIcon />;
+    }
+
+    return null;
+  };
+
+  return <Pressable onPress={onPress}>{renderIcon()}</Pressable>;
 };
 
 export default Check;
