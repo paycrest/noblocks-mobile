56353f32f0a1f931de2d8450ddfe2fef54df2996
marvelous-ikechi <marvelike7@gmail.com>
2026-04-28 13:37:52 +0100

    feat: remove hardcoded host configuration from Reactotron setup

--- ReactotronConfig.js
+++ ReactotronConfig.js
@@ -2,7 +2,6 @@ import Reactotron from "reactotron-react-native";
 
 Reactotron.configure({
   name: "noblocks",
-  host: "192.168.1.2",
 }) // controls connection & communication settings
   .useReactNative() // add all built-in react native plugins
   .connect(); // let's connect!
